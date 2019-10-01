820c61c6c2c8d93263c7704a78fb28d578ab5479
denderov <denderov@gmail.com>
2019-10-02 00:38:28 +0300

    added test for guava method.

---
+++ hw01-maven/src/test/java/GuavaTest.java
@@ -0,0 +1,23 @@
+import com.google.common.collect.Iterables;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import static org.junit.Assert.assertEquals;
+
+public class GuavaTest {
+
+    @Test
+    public void isNullsRemovedFromList() {
+        List<String> strings = new ArrayList<>();
+        strings.add("one");
+        strings.add("two");
+        strings.add(null);
+        strings.add("three");
+        Iterables.removeIf(strings, Objects::isNull);//Predicates.isNull()
+        assertEquals(3,strings.size());
+
+    }
+}
