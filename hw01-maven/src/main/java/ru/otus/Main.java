da2c571a5232c046eeabb08ec094bf682d409f2b
denderov <denderov@gmail.com>
2019-10-04 19:45:57 +0300

    cleaning classes, editing pom for fat-jar.

--- hw01-maven/src/main/java/ru/otus/Main.java
+++ hw01-maven/src/main/java/ru/otus/Main.java
@@ -1,20 +1,12 @@
 package ru.otus;
 
-//import com.google.common.collect.Iterables;
-
 import com.google.common.base.Splitter;
-import com.google.common.collect.Iterables;
-
-import java.util.ArrayList;
 import java.util.List;
-//import java.util.Objects;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        int count;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter sentence:");
         String sentence = scanner.nextLine();
