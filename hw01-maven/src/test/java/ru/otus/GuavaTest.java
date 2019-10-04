875957151873f27c29f6d9d9f8ec55430be9f21c
denderov <denderov@gmail.com>
2019-10-04 20:49:56 +0300

    cleaning unused imports test class.

--- hw01-maven/src/test/java/ru/otus/GuavaTest.java
+++ hw01-maven/src/test/java/ru/otus/GuavaTest.java
@@ -1,12 +1,8 @@
 package ru.otus;
 
-import com.google.common.collect.Iterables;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-
 
 import static org.hamcrest.Matchers.contains;
 import static org.junit.Assert.assertEquals;
