5876e0be6219f39bfc8c37cf5eae8b4e394a0c04
denderov <denderov@gmail.com>
2019-10-08 00:02:51 +0300

    test class OriginalArrayListTest modified. added checks for Collections methods.

--- hw02-diyarraylist/src/test/java/ru/otus/OriginalArrayListTest.java
+++ hw02-diyarraylist/src/test/java/ru/otus/OriginalArrayListTest.java
@@ -4,6 +4,8 @@ import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
@@ -16,5 +18,45 @@ public class OriginalArrayListTest {
         integerArrayList.add(1);
         assertEquals(1,integerArrayList.size());
         assertThat(integerArrayList, CoreMatchers.hasItem(1));
+    }
+
+    @Test
+    public void checkCollectionsAddAll() {
+        Integer[] integerArray = getIntegers();
+        ArrayList<Integer> integerArrayList = new ArrayList<>();
+        Collections.addAll(integerArrayList, integerArray);
+        assertEquals(20,integerArrayList.size());
+        assertThat(integerArrayList, CoreMatchers.hasItems(integerArray));
+    }
+
+    @Test
+    public void checkCollectionsCopy() {
+        Integer[] integerArray = getIntegers();
+        Integer[] reverseIntegerArray = getReverseIntegers();
+        ArrayList<Integer> integerArrayList = new ArrayList<>();
+        Collections.addAll(integerArrayList, integerArray);
+        ArrayList<Integer> copyIntegerArrayList = new ArrayList<>();
+        Collections.addAll(copyIntegerArrayList, reverseIntegerArray);
+        Collections.copy(copyIntegerArrayList,integerArrayList);
+        assertEquals(20,integerArrayList.size());
+        assertThat(integerArrayList, CoreMatchers.hasItems(integerArray));
+        assertEquals(20, copyIntegerArrayList.size());
+        assertThat(copyIntegerArrayList, CoreMatchers.hasItems(integerArray));
+    }
+
+    private Integer[] getReverseIntegers() {
+        Integer[] reverseIntegerArray = new Integer[20];
+        for (int i = 0; i <20; i++) {
+            reverseIntegerArray[i] = 19-i;
+        }
+        return reverseIntegerArray;
+    }
+
+    private Integer[] getIntegers() {
+        Integer[] integerArray = new Integer[20];
+        for (int i = 0; i <20; i++) {
+            integerArray[i] = i;
+        }
+        return integerArray;
     }
 }
