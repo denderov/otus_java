e44cb89bb937a44ec50adf7cfc8015eaea50a446
denderov <denderov@gmail.com>
2019-10-17 23:09:30 +0300

    prepare to package.

--- hw03-gc/src/main/java/ru/otus/gc/Misc.java
+++ hw03-gc/src/main/java/ru/otus/gc/Misc.java
@@ -1,5 +1,6 @@
 package ru.otus.gc;
 
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -7,8 +8,14 @@ public class Misc {
 
     private static Logger logger = Logger.getLogger(Misc.class.getName());
 
-    public static void main(String[] args) {
-        logExamples();
+    public static void main(String[] args) throws InterruptedException {
+//        logExamples();
+        for (int i = 0; i < 1000000; i++) {
+            Integer[] integers = new Integer[1000];
+            Arrays.fill(integers,i);
+            Thread.sleep(100);
+            System.out.println(i);
+        }
     }
 
     private static void logExamples() {
