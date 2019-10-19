befce8db83dc2a6203fa8406726eccf785e94717
denderov <denderov@gmail.com>
2019-10-20 00:00:28 +0300

    parameters corrected.

--- hw03-gc/src/main/java/ru/otus/gc/Misc.java
+++ hw03-gc/src/main/java/ru/otus/gc/Misc.java
@@ -14,9 +14,9 @@ public class Misc {
 //        command line argument for remote
 //        -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005
         for (int i = 0; i < 1000000; i++) {
-            Integer[] integers = new Integer[300000];
-            Arrays.fill(integers,i);
-            Thread.sleep(1000);
+            Integer[] integers = new Integer[50000];
+            Arrays.fill(integers,new Integer(i));
+            Thread.sleep(100);
             System.out.println(i);
         }
     }
