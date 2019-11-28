bb53752bcd3155ba8aa3c06206d2061473c253a1
denderov <denderov@gmail.com>
2019-11-28 07:09:38 +0300

    class for testing core implementation.

---
+++ hw05-annotations/src/main/java/ru/otus/hw05/TestingClass.java
@@ -0,0 +1,61 @@
+package ru.otus.hw05;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestingClass {
+
+
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException, InvocationTargetException {
+
+        String className = "ru.otus.hw05.ClassForTesting";
+
+        Class classForTesting = Class.forName(className);
+
+        List<Method> testingMethods = new ArrayList<>();
+        List<Method> beforeMethods = new ArrayList<>();
+        List<Method> afterMethods = new ArrayList<>();
+
+        for (Method method : classForTesting.getDeclaredMethods()) {
+            for (Annotation annotation : method.getDeclaredAnnotations()) {
+
+                if (annotation instanceof Test) {
+                    testingMethods.add(method);
+
+                } else if (annotation instanceof Before) {
+                    beforeMethods.add(method);
+
+                } else if (annotation instanceof After) {
+                    afterMethods.add(method);
+
+                }
+
+            }
+        }
+
+        for (Method method : testingMethods) {
+
+            var instanceOfClassForTesting = classForTesting.newInstance();
+
+            for (Method beforeMethod : beforeMethods) {
+                System.out.println(beforeMethod.getName());
+                beforeMethod.invoke(instanceOfClassForTesting);
+            }
+
+            method.invoke(instanceOfClassForTesting);
+            System.out.println(method.getName());
+
+            for (Method afterMethod : afterMethods) {
+                System.out.println(afterMethod.getName());
+                afterMethod.invoke(instanceOfClassForTesting);
+            }
+
+        }
+
+    }
+
+
+}
