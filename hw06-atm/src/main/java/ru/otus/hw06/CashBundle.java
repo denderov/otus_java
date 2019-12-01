f87e12b9d7db9ee6a62f26485afb662138081328
denderov <denderov@gmail.com>
2019-12-02 00:58:16 +0300

    CashBundle class added.

---
+++ hw06-atm/src/main/java/ru/otus/hw06/CashBundle.java
@@ -0,0 +1,58 @@
+package ru.otus.hw06;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class CashBundle {
+
+    private SortedMap<Integer, Integer> moneybox = new TreeMap<>();
+
+    public CashBundle get(int sum) {
+
+        CashBundle outCashBundle = new CashBundle();
+
+        SortedMap<Integer, Integer> reversedMoneybox = new TreeMap<>(Collections.reverseOrder());
+
+        reversedMoneybox.putAll(moneybox);
+
+        for (Map.Entry<Integer, Integer> entry : reversedMoneybox.entrySet()) {
+
+            int faceValue = entry.getKey();
+            int noteCount = entry.getValue();
+
+            int quotient = sum / faceValue;
+
+            int withdrawNoteCount = Math.min(noteCount, quotient);
+
+            moneybox.merge(faceValue, -withdrawNoteCount, Integer::sum);
+            outCashBundle.put(faceValue, withdrawNoteCount);
+
+            sum -= withdrawNoteCount * faceValue;
+
+        }
+
+        return outCashBundle;
+    }
+
+    public CashBundle put(int faceValue, int noteCount) {
+
+        moneybox.merge(faceValue, noteCount, Integer::sum);
+
+        return this;
+
+    }
+
+    @Override
+    public String toString() {
+
+        if (moneybox.isEmpty()) {
+            return "Cash bundle is empty";
+
+        } else {
+            return "Cash bundle: "+ moneybox;
+        }
+
+    }
+}
---
+++ hw06-atm/src/test/java/ru/otus/hw06/CashBundleTest.java
@@ -0,0 +1,36 @@
+package ru.otus.hw06;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+class CashBundleTest {
+
+    @Test
+    void toStringEmptyCashBundleCheck() {
+        CashBundle cashBundle = new CashBundle();
+        assertThat(cashBundle.toString(), equalTo("Cash bundle is empty"));
+    }
+
+    @Test
+    void toStringNonEmptyCashBundleCheck() {
+        CashBundle cashBundle = new CashBundle()
+                .put(200, 100)
+                .put(100, 1000)
+                .put(200, 100);
+        assertThat(cashBundle.toString(), equalTo("Cash bundle: {100=1000, 200=200}"));
+    }
+
+    @Test
+    void getCashBundleCheck() {
+        CashBundle cashBundle = new CashBundle()
+                .put(200, 100)
+                .put(100, 1000)
+                .put(200, 100);
+        CashBundle cashBundleReceived = cashBundle.get(500);
+        assertThat(cashBundle.toString(), equalTo("Cash bundle: {100=999, 200=198}"));
+        assertThat(cashBundleReceived.toString(), equalTo("Cash bundle: {100=1, 200=2}"));
+    }
+
+}
