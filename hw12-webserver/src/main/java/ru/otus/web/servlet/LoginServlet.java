11229d7173102a61a15c2370064d87fbe0b625b4
denderov <denderov@gmail.com>
2020-01-28 07:38:41 +0300

    revert hw12
    Signed-off-by: denderov <denderov@gmail.com>

--- hw12-webserver/src/main/java/ru/otus/web/servlet/LoginServlet.java
+++ hw12-webserver/src/main/java/ru/otus/web/servlet/LoginServlet.java
@@ -19,6 +19,7 @@ public class LoginServlet extends HttpServlet {
     private static final int MAX_INACTIVE_INTERVAL = 30;
     private static final String LOGIN_PAGE_TEMPLATE = "login.html";
 
+
     private final TemplateProcessor templateProcessor;
     private final UserAuthService userAuthService;
 
