8c02a87e2674e2b6bb8f61c4d6b6fa9dd1527fd2
denderov <denderov@gmail.com>
2020-02-02 15:49:46 +0300

    duplicate fix
    Signed-off-by: denderov <denderov@gmail.com>

--- hw13-ioc/src/main/java/ru/otus/config/AppConfig.java
+++ hw13-ioc/src/main/java/ru/otus/config/AppConfig.java
@@ -2,9 +2,7 @@ package ru.otus.config;
 
 import org.hibernate.SessionFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import ru.otus.api.model.User;
 import ru.otus.api.service.DBServiceUser;
@@ -13,8 +11,6 @@ import ru.otus.hibernate.HibernateUtils;
 import ru.otus.web.helpers.DBInitializer;
 
 @Configuration
-@ComponentScan("ru.otus")
-@EnableWebMvc
 public class AppConfig implements WebMvcConfigurer {
     @Bean(initMethod="init")
     public DBInitializer initUsers(DBServiceUser dbServiceUser) {
