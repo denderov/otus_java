842c5db78a9aa6287cf23694824b37e0a0b06575
denderov <denderov@gmail.com>
2020-02-12 07:07:18 +0300

    js and controller from gs-messaging-stomp-websocket (spring guide)
    Signed-off-by: denderov <denderov@gmail.com>

---
+++ hw15-messageSystem/src/main/resources/static/app.js
@@ -0,0 +1,56 @@
+var stompClient = null;
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+    $("#greetings").html("");
+}
+
+function connect() {
+    var socket = new SockJS('/websocket-endpoint');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/frontend/get_user', function (user) {
+            showGreeting(JSON.parse(greeting.body).user);
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendName() {
+    stompClient.send("/app/hello", {}, JSON.stringify({'name': $("#name").val()}));
+}
+
+function addUser(json) {
+    $("#users").append(
+        "<td>" + JSON.parse(json).id + "</td>" +
+        "<td>" + JSON.parse(json).name + "</td>" +
+        "<td>" + JSON.parse(json).login + "</td>" +
+        "<td>" + JSON.parse(json).password + "</td>"
+        );
+}
+
+$(function () {
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+    $( "#connect" ).click(function() { connect(); });
+    $( "#disconnect" ).click(function() { disconnect(); });
+    $( "#send" ).click(function() { sendName(); });
+});
+
